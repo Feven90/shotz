97ca943948b5f6f78a381e9cb662242f40002970
Feven Mulugeta <f.mulugeta90@gmail.com>
2018-10-22 18:39:29 -0500

    in-progress

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -1,13 +1,12 @@
-
 const movieLocation = (locationArray) => {
     let newString= "";
-    locationArray.forEach((location,i) => {
-    newString += `<div id="locations" class="card location" style="width: 18rem;">`;
+    locationArray.forEach(location => {
+    newString += `<div class="card location card-deck" style="width: 18rem;">`;
     newString += `<img  class="card-img-top" src=${location.image} width="200" height="200">`;
     newString += `<div class="card-body">`;
     newString += `<h5 class="card-title location-name">${location.name}</h5>`;
-    newString += `<p class="card-text">${location.address}<p>`;
-    newString += `<p class="card-text">${location.time}<p>`;
+    newString += `<h6 class="card-text">${location.address}<6>`;
+    newString += `<p class="card-text time">${location.time}<p>`;
     newString += `</div>`;
     newString += `</div>`
     
@@ -15,43 +14,37 @@ const movieLocation = (locationArray) => {
 // print to DOM 
 $("#location").append(newString);
 }
-
-
-// let Locations= document.getElementsByClassName("card location");
-// console.log(Locations);
-// console.log(Locations.length);
-// // $("h5").each(function( index ) {
-// //     console.log( $( this ).text() );
-// //   });
+// let allLocations= document.getElementsByClassName("location-name");
+// console.log(allLocations);
 const shotzLocation = (userInput) => {
-    $(".location-name").text((i, text) => {
-        console.log(text);
+    $(".location").each((i,location) => {
+        const allLocations = $(location).find("h5").html();
         console.log(userInput);
-        if (text === userInput) {
-            $(".location").addClass("show");
-            $(".location").not(".show").hide(); 
-            return false;       
-        }
-        // else {
-        //     return "Show Sale Fish";
-        // }
-        // return (text === "Show Sale Fish") ? 'Show All' : 'Show Sale Fish'
-    });
-//     $(".location").each((i,location) => {
-//         const allLocations = $(location).find("h5").html();
-//         console.log(userInput);
-//         console.log(allLocations.toLowerCase().includes(userInput.toLowerCase()));
-//     //             if (allLocations.toLowerCase().includes(userInput.toLowerCase())) {
-//     //             $(".location").addClass("show");
-//     //                 $(".location").not(".show").hide(); 
-//     //                 return false;
-//     // }
-//     if ( $( this ).is(".card location" ) ) {
-//         $(".location").addClass("show");
-//                     $(".location").not(".show").hide(); 
-//                     return false;
-//     }
-//         console.log(allLocations);
-// });
+        console.log(allLocations.toLowerCase().includes(userInput.toLowerCase()));
+                if (allLocations.toLowerCase().includes(userInput.toLowerCase())) {
+                // $(".location").addClass("show");
+                $(location).find("div");
+                // console.log($("#location .location").addClass("show"));
+                    // $(location).remove(); 
+                // $(".location").toggle();
+    }
+    else {
+        // $("location").removeClass("show");
+        $(location).remove(); 
+
+    }
+        console.log(allLocations);
+});
 }
+const filter =() => {
+$("#filter button").on("click", function(){
+    console.log("you clicked a button");
+    var filtertag = $(this).attr('class');
+    // $('.time').hide().filter('.' + filtertag).show();
+    $('location').show(); // Show all posts
+console.log($('p.text():not(.' + filtertag + ')'));
+$('p:not(.' + filtertag + ')').hide();
+})
+}
+filter();
 export {movieLocation,shotzLocation}
