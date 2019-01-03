ecdef4a4c447e132ce927f721a01b42233686e9b
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2019-01-02 23:08:30 -0600

    in-progress

--- javascripts/components/movieComponent.js
+++ javascripts/components/movieComponent.js
@@ -10,10 +10,8 @@ const myMovie = (movieArray) => {
     let newString= "";
     movieArray.forEach(movie => {
     newString += `<div id="${movie.id}" class="movie card" style="width: 18rem;">`;
-    newString += `<img  class="movie-image card-img-top" src=${movie.image} width="200" height="200">`;
-    newString += `</div>`;
-    newString += `<div class="content">`
-    newString += `<h3 class="card-title">${movie.name}</h3>`;
+    // newString += `<img  class="movie-image card-img-top" src=${movie.image} width="200" height="200">`;
+    newString += `<h3 class="card-header">${movie.name}</h3>`;
     newString += `<p class="card-text">${movie.genere}<p>`;
     newString += `<p class="card-text">${movie.estimated}<p>`;
     newString += `<p class="card-text">${movie.description}<p>`;
